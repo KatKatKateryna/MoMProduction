7dd2368ae68896ff183735856a9ae9f4ca3b517d
Kateryna Konieva <katrinkonieva@gmail.com>
2026-03-24 23:33:40 +0000

    main

--- first_setup/setup.sh
+++ first_setup/setup.sh
@@ -9,7 +9,7 @@ set -o pipefail
 
 REPO_URL="https://github.com/KatKatKateryna/MoMProduction.git"
 REPO_DIR="$HOME/MoMProduction"
-REPO_BRANCH="dev"
+REPO_BRANCH="main"
 if command -v conda >/dev/null 2>&1; then
     CONDA_DIR="$(conda info --base)"
 else
