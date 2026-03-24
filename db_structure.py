244220b7373b6cca0c3c74fb2eb515886e1e64ad
Kateryna Konieva <katrinkonieva@gmail.com>
2026-03-24 18:48:31 +0000

    url

--- db_structure.py
+++ db_structure.py
@@ -1,4 +1,6 @@
 
+## Server: https://mom.tg-ear190027.projects.jetstream-cloud.org/ModelofModels/ 
+
 ## Final_Alert (4 csvs per day)
 alert_labels = ',pfaf_id,name,name_1,CentroidX,CentroidY,Admin1_count,Admin1_names,area_km2,rfr_score,cfr_score,Alert_level,Days_until_peak,GloFAS_2yr,GloFAS_5yr,GloFAS_20yr,Alert_Score,PeakArrivalScore,TwoYScore,FiveYScore,TwtyYScore,Sum_Score_x,GFMS_TotalArea_km,GFMS_perc_Area,GFMS_MeanDepth,GFMS_MaxDepth,GFMS_Duration,GFMS_area_score,GFMS_perc_area_score,MeanD_Score,MaxD_Score,Duration_Score,Sum_Score_y,MOM_Score,Hazard_Score,Scaled_Riverine_Risk,Scaled_Coastal_Risk,Flag,1-Day_TotalArea_km2,1-Day_perc_Area,1-Day_CS_TotalArea_km2,1-Day_CS_perc_Area,2-Day_TotalArea_km2,2-Day_perc_Area,3-Day_TotalArea_km2,3-Day_perc_Area,DFO_area_1day_score,DFO_percarea_1day_score,DFO_area_2day_score,DFO_percarea_2day_score,DFO_area_3day_score,DFO_percarea_3day_score,DFOTotal_Score,onedayFlood_Area_km,onedayperc_Area,fivedayFlood_Area_km,fivedayperc_Area,VIIRS_area_1day_score,VIIRS_percarea_1day_score,VIIRS_area_5day_score,VIIRS_percarea_5day_score,VIIRSTotal_Score,Severity,Alert,Status'.split(",")
 
